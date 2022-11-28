8c2d9075aa8f6b0c57dcb4f008d065717da301ae
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-28 10:32:21 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define fi first
+#define se second
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
