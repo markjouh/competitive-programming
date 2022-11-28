ff7384e88ce874a681bd88e0b3f3b5480171eb12
Mark Zhou <xinyunzhouu@gmail.com>
2022-11-28 10:31:54 -0500

    Update prims.cpp

--- graphs/prims.cpp
+++ graphs/prims.cpp
@@ -1,18 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-#define fi first
-#define se second
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+void prims {
     int n, m;
     cin >> n >> m;
     vector<vector<pii>> g(n);
