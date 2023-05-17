eee53fc16e4098bf3aef544608f41dd0070050a4
Mark Zhou <xinyunzhouu@gmail.com>
2023-05-16 21:14:46 -0400

    Update purq_segtree.cpp

--- segtree/purq_segtree.cpp
+++ segtree/purq_segtree.cpp
@@ -1,6 +1,12 @@
-struct segtree {
+struct Tree {
+    typedef int T;
+
+    T f(T a, T b) {
+        return min(a, b);
+    }
+
     int siz;
-    vector<ll> tree;
+    vector<T> tree;
 
     void init(int n) {
         siz = 1;
@@ -21,14 +27,14 @@ struct segtree {
         } else {
             upd(i, v, 2 * x + 2, mid, r);
         }
-        tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
+        tree[x] = f(tree[2 * x + 1], tree[2 * x + 2]);
     }
 
     void upd(int i, int v) {
         upd(i, v, 0, 0, siz);
     }
 
-    void build(vector<int> &a, int x, int l, int r) {
+    void build(vector<T> &a, int x, int l, int r) {
         if (l + 1 == r) {
             if (l < sz(a)) {
                 tree[x] = a[l];
@@ -38,14 +44,15 @@ struct segtree {
         int mid = l + (r - l) / 2;
         build(a, 2 * x + 1, l, mid);
         build(a, 2 * x + 2, mid, r);
-        tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
+        tree[x] = f(tree[2 * x + 1], tree[2 * x + 2]);
     }
 
-    void build(vector<int> &a) {
+    void build(vector<T> &a) {
+        init(sz(a));
         build(a, 0, 0, siz);
     }
 
-    ll query(int L, int R, int x, int l, int r) {
+    T query(int L, int R, int x, int l, int r) {
         if (l >= R || r <= L) {
             return 0;
         }
@@ -53,10 +60,10 @@ struct segtree {
             return tree[x];
         }
         int mid = l + (r - l) / 2;
-        return query(L, R, 2 * x + 1, l, mid) + query(L, R, 2 * x + 2, mid, r);
+        return f(query(L, R, 2 * x + 1, l, mid), query(L, R, 2 * x + 2, mid, r));
     }
 
-    ll query(int l, int r) {
+    T query(int l, int r) {
         return query(l, r, 0, 0, siz);
     }
 };
