040d51de02c0ee2ccb8403e0ca657cfa4b54df74
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-17 14:19:36 -0400

    Update Pragmas.h

--- misc/Pragmas.h
+++ misc/Pragmas.h
@@ -1,4 +1,11 @@
-// https://codeforces.com/blog/entry/96344?#comment-854236
-// Change Ofast to O3 if doing floating point arithmetic
+/**
+ * Author: Sergey Slotin
+ * Date: 26 October 2021
+ * Description: Auto-vectorization, may speed up code.
+ * Change Ofast to O3 if working with floating point arithmetic.
+ * Verification: https://codeforces.com/contest/1720/submission/219193438
+ * Time: O(1) update and access
+ */
+
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("avx2,tune=native")
