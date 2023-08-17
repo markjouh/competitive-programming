baf1907641b31ac58c28fc585c85f41627deb1a7
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-17 14:19:54 -0400

    Update Pragmas.h

--- misc/Pragmas.h
+++ misc/Pragmas.h
@@ -4,7 +4,7 @@
  * Description: Auto-vectorization, may speed up code.
  * Change Ofast to O3 if working with floating point arithmetic.
  * Verification: https://codeforces.com/contest/1720/submission/219193438
- * Time: O(1) update and access
+ * Time: N/A
  */
 
 #pragma GCC optimize("Ofast,unroll-loops")
