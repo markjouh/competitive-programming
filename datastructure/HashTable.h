5bb6e0c61ee12c5eabb9ff07c14ce84884244913
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-17 14:22:35 -0400

    Update HashTable.h

--- datastructure/HashTable.h
+++ datastructure/HashTable.h
@@ -1,6 +1,6 @@
 /**
  * Author: Neal Wu
- * Date: 17 August 2023
+ * Date: 11 October 2018
  * Description: Much faster unordered_map using GNU pbds, paired with a hack-resistant hash function.
  * Verification: https://codeforces.com/contest/1642/submission/218073453
  * Time: O(1) update and access
