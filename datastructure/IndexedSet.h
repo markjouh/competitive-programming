0e620bf3d4c559726d4bbb4dd4a248de14c89057
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-17 14:05:39 -0400

    Update and rename IndexedSet.h to IndexedSet.h

---
+++ datastructure/IndexedSet.h
@@ -0,0 +1,13 @@
+/**
+ * Author: Mark Zhou
+ * Date: 15 August 2023
+ * Description: Supports queries for the kth largest element and for the number of elements less than k in log time.
+ * Verification: https://codeforces.com/contest/1860/problem/C
+ * Time: O(log(n)) insert and query
+ */
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+// find_by_order(k) - The item at index k
+// order_of_key(k) - Count items < k
+template <typename T> using indexed_set = __gnu_pbds::tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
--- misc/IndexedSet.h
+++
@@ -1,5 +0,0 @@
-#include <ext/pb_ds/assoc_container.hpp>
-
-// find_by_order(k) - The item at index k
-// order_of_key(k) - Count items < k
-template <typename T> using indexed_set = __gnu_pbds::tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
