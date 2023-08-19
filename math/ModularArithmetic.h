dcd22a27fc235a345e580032980b8826d6bf270b
Mark Zhou <xinyunzhouu@gmail.com>
2023-08-19 12:37:18 -0400

    Start adding number theory and modulo stuff

---
+++ math/ModularArithmetic.h
@@ -0,0 +1,39 @@
+ll mod_exp(ll base, int exp) {
+    ll res = 1;
+    while (exp != 0) {
+        if (exp & 1) {
+            res = res * base % MOD;
+        }
+        base = base * base % MOD;
+        exp >>= 1;
+    }
+    return res;
+}
+
+ll mod_inv(int x) {
+    return mod_exp(x, MOD - 2);
+}
+
+vector<ll> fact, inv_fact;
+
+void gen_factorial(int n) {
+    fact.resize(n + 1);
+    inv_fact.resize(n + 1);
+    fact[0] = fact[1] = inv_fact[0] = inv_fact[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+
+    inv_fact[n] = mod_inv(fact[n]);
+    for (int i = n - 1; i >= 2; i--) {
+        inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
+    }
+}
+
+ll choose(int n, int k) {
+    if (k < 0 || k > n) {
+        return 0;
+    }
+    return fact[n] * inv_fact[k] % MOD * inv_fact[n - k] % MOD;
+}
---
+++ math/NumberTheory.h
@@ -0,0 +1,24 @@
+<int> mn_factor;
+vector<bool> prime;
+vector<int> primes;
+
+void sieve(int n) {
+    mn_factor.assign(n + 1, 0);
+    prime.assign(n + 1, true);
+    prime[0] = prime[1] = false;
+    primes.clear();
+
+    for (int p = 2; p <= n; p++) {
+        if (prime[p]) {
+            mn_factor[p] = p;
+            primes.push_back(p);
+
+            for (ll i = 1ll * p * p; i <= n; i += p) {
+                if (prime[i]) {
+                    prime[i] = false;
+                    mn_factor[i] = p;
+                }
+            }
+        }
+    }
+}
