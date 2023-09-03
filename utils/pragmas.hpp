fa01d02cccf9e92517ffdc49cef9d54ae4cca76d
Mark Zhou <xinyunzhouu@gmail.com>
2023-09-03 01:50:42 +0000

    Delete utils/pragmas.hpp

--- utils/pragmas.hpp
+++
@@ -1,8 +0,0 @@
-/**
- * Description: Auto-vectorization, may speed up code.
- * Change Ofast to O3 if working with floating point arithmetic.
- * Verification: https://codeforces.com/contest/1720/submission/219193438
- */
-
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx2,tune=native")
