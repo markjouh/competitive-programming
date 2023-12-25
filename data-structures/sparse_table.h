c79a9b204cb53a513f2d51ac689a91c91a7caef9
Mark Zhou <xinyunzhouu@gmail.com>
2023-12-25 18:32:47 -0500

    Update sparse_table.h

--- data-structures/sparse_table.h
+++ data-structures/sparse_table.h
@@ -1,12 +1,11 @@
-template <typename T>
 struct RMQ {
-    vector<vector<T>> table;
+    vector<vector<int>> table;
 
-    T merge(T a, T b) { // idempotent fn
+    int merge(int a, int b) {
         return min(a, b);
     }
 
-    RMQ(vector<T> &a) : table(__lg(a.size()) + 1, vector<T>(a.size())) {
+    RMQ(vector<int> &a) : table(__lg(a.size()) + 1, vector<int>(a.size())) {
         table[0] = a;
         for (int i = 1; i <= __lg(a.size()); i++) {
             for (int j = 0; j + (1 << i) <= a.size(); j++) {
@@ -15,7 +14,7 @@ struct RMQ {
         }
     }
 
-    T query(int l, int r) {
+    int query(int l, int r) {
         int i = __lg(r - l);
         return merge(table[i][l], table[i][r - (1 << i)]);
     }
