ec70cef52f2c0b01b8a2a2149ab0641f0823c5d4
Mark Zhou <xinyunzhouu@gmail.com>
2023-12-25 18:05:30 -0500

    Update number_theory.hpp

--- number_theory.hpp
+++ number_theory.hpp
@@ -1,60 +1,73 @@
-vector<int> mn_factor;
-vector<bool> prime;
-vector<int> primes;
+namespace number_theory {
+    vector<int> mn_factor;
+    vector<bool> prime;
+    vector<int> primes;
 
-void sieve(int n) {
-    mn_factor.assign(n + 1, 0);
-    prime.assign(n + 1, true);
-    prime[0] = prime[1] = false;
-    primes.clear();
+    void sieve(int n) {
+        mn_factor.resize(n + 1);
+        prime.resize(n + 1, true);
+        prime[0] = prime[1] = false;
 
-    for (int p = 2; p <= n; p++) {
-        if (prime[p]) {
-            mn_factor[p] = p;
-            primes.push_back(p);
+        for (int i = 2; i <= n; i++) {
+            if (prime[i]) {
+                mn_factor[i] = i;
+                primes.push_back(i);
 
-            for (int64_t i = 1ll * p * p; i <= n; i += p) {
-                if (prime[i]) {
-                    prime[i] = false;
-                    mn_factor[i] = p;
+                for (long long j = 1ll * i * i; j <= n; j += i) {
+                    if (prime[j]) {
+                        prime[j] = false;
+                        mn_factor[j] = i;
+                    }
                 }
             }
         }
     }
-}
 
-vector<pair<int, int>> prime_factorize(int n) {
-    vector<pair<int, int>> res;
-    for (int p : primes) {
-        if (1ll * p * p > n) {
-            break;
-        }
-        if (n % p == 0) {
-            res.push_back({p, 0});
-            do {
-                n /= p;
+    vector<pair<int, int>> factorize(int n) {
+        vector<pair<int, int>> res;
+
+        if (n <= mn_factor.size()) {
+            while (n != 1) {
+                if (res.empty() || res.back().first != mn_factor[n]) {
+                    res.push_back({mn_factor[n], 0});
+                }
+                n /= mn_factor[n];
                 res.back().second++;
-            } while (n % p == 0);
+            }
+            return res;
         }
-    }
-    if (n > 1) {
-        res.push_back({n, 1});
-    }
-    return res;
-}
 
-vector<int> get_factors(int n) {
-    vector<int> res = {1};
-    for (auto [p, exp] : prime_factorize(n)) {
-        const int prev_sz = size(res);
-        
-        int mul = 1;
-        for (int i = 0; i < exp; i++) {
-            mul *= p;
-            for (int j = 0; j < prev_sz; j++) {
-                res.push_back(res[j] * mul);
+        for (int p : primes) {
+            if (1ll * p * p > n) {
+                break;
+            }
+            if (n % p == 0) {
+                res.push_back({p, 0});
+                while (n % p == 0) {
+                    n /= p;
+                    res.back().second++;
+                }
             }
         }
+        if (n > 1) {
+            res.push_back({n, 1});
+        }
+        return res;
     }
-    return res;
-}
+
+    vector<int> get_factors(int n) {
+        vector<int> res;
+        res.push_back(1);
+        for (auto [base, exp] : factorize(n)) {
+            int old_sz = res.size();
+            int cur = 1;
+            for (int i = 0; i < exp; i++) {
+                cur *= base;
+                for (int j = 0; j < old_sz; j++) {
+                    res.push_back(res[j] * cur);
+                }
+            }
+        }
+        return res;
+    }
+};
