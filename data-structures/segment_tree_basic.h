ff7a1c51eee8df92568688fdaf9dcba9ee892deb
Mark Zhou <xinyunzhouu@gmail.com>
2023-12-25 18:37:07 -0500

    Update segment_tree_basic.h

--- data-structures/segment_tree_basic.h
+++ data-structures/segment_tree_basic.h
@@ -1,10 +1,11 @@
+template<typename T>
 struct ST {
     int n;
-    vector<int> tree;
+    vector<T> tree;
     ST(int _n) : n(_n), tree(2 * n) {}
  
-    int query(int l, int r) {
-        int ret = 0;
+    T query(int l, int r) {
+        T ret = 0;
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
             if (l & 1) ret += tree[l++];
             if (r & 1) ret += tree[--r];
@@ -12,7 +13,7 @@ struct ST {
         return ret;
     }
  
-    void update(int p, int v) {
+    void update(int p, T v) {
         for (tree[p += n] = v, p >>= 1; p > 0; p >>= 1) {
             tree[p] = tree[p << 1] + tree[p << 1 | 1];
         }
