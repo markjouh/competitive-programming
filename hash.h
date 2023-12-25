21222b282bd1658c9e92749a8e7c395636f29ce4
Mark Zhou <xinyunzhouu@gmail.com>
2023-12-25 18:14:04 -0500

    Update and rename hashing.hpp to hash.h

--- hash.h
+++ hash.h
@@ -1,26 +1,26 @@
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int64_t MOD = (1ll << 61) - 1;
-const int64_t B = uniform_int_distribution<int64_t>(1e3, MOD - 1e3)(rng);
+const long long MOD = (1ll << 61) - 1;
+const long long B = uniform_int_distribution<long long>(1e3, MOD - 1e3)(rng);
 
-vector<int64_t> pow_b = {1};
+vector<long long> pow_b = {1};
 
 template <typename T>
 struct Hash {
     T s;
-    vector<int64_t> h;
+    vector<long long> h;
 
     Hash(T _s) : s(_s) {
         h.resize(sz(s) + 1);
         for (int i = 0; i < sz(s); i++) {
             h[i + 1] = (__int128_t(h[i]) * B + s[i]) % MOD;
         }
-        while (sz(pow_b) < sz(h)) {
-            pow_b.pb(__int128_t(pow_b.back()) * B % MOD);
+        while (pow_b.size() < h.size()) {
+            pow_b.push_back(__int128_t(pow_b.back()) * B % MOD);
         }
     }
 
-    int64_t get_hash(int l, int r) {
+    long long get_hash(int l, int r) {
         return ((h[r] - __int128_t(h[l]) * pow_b[r - l]) % MOD + MOD) % MOD;
     };
 };
