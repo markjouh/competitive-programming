ef84ce00cda34af945c4ba4f20be683720f499eb
Mark Zhou <xinyunzhouu@gmail.com>
2023-12-26 16:52:37 -0500

    Update and rename segment_tree_basic.h to segment_tree_purq.h

--- data-structures/segment_tree_purq.h
+++ data-structures/segment_tree_purq.h
@@ -2,7 +2,17 @@ template<typename T>
 struct ST {
     int n;
     vector<T> tree;
+
     ST(int _n) : n(_n), tree(2 * n) {}
+
+    ST(vector<T> &a) : n(a.size()), tree(2 * n) {
+        for (int i = 0; i < n; i++) {
+            tree[i + n] = a[i];
+        }
+        for (int i = n - 1; i > 0; i--) {
+            tree[i] = tree[i << 1] + tree[i << 1 | 1];
+        }
+    }
  
     T query(int l, int r) {
         T ret = 0;
