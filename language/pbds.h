744824d0dad79bdb1fd6e6abbb823a9feab8c013
Mark Zhou <xinyunzhouu@gmail.com>
2023-12-28 10:51:53 -0500

    Update pbds.h

--- language/pbds.h
+++ language/pbds.h
@@ -1,11 +1,6 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
 using namespace __gnu_pbds;
-
-// find_by_order(k) - The item at index k
-// order_of_key(k) - Count items < k
-template<typename T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
@@ -22,4 +17,7 @@ struct custom_hash {
     }
 };
 
+// find_by_order(k) - The item at index k
+// order_of_key(k) - Count items < k
+template<typename T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename K, typename V> using hash_table = gp_hash_table<K, V, custom_hash>;
