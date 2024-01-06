111cd1eb116c800669fbda68b8594d8df0c42cb7
Mark Zhou <xinyunzhouu@gmail.com>
2024-01-06 02:13:56 -0500

    Update template.cpp

--- language/template.cpp
+++ language/template.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define int int64_t
+
+#define rng(i, a, b) for (int i = a; i < b; i++)
+#define rep(i, b) rng(i, 0, b)
+#define gnr(i, a, b) for (int i = b - 1; i >= a; i--)
+#define per(i, b) gnr(i, 0, b)
+
+#define all(x) begin(x), end(x)
+#define sz(x) int(size(x))
+
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+#define f first
+#define s second
+
+int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
