a438501dccf4c603d1912d85b3177d6f46400694
Mark Zhou <xinyunzhouu@gmail.com>
2024-01-06 02:17:09 -0500

    Update modular_arithmetic.h

--- math/modular_arithmetic.h
+++ math/modular_arithmetic.h
@@ -1,16 +1,16 @@
-namespace modular {
+namespace modop {
     const int MOD = 998244353;
 
-    int madd(int a, int b) {
-        return (0ll + a + b) % MOD;
+    int madd(int64_t a, int64_t b) {
+        return (a + b) % MOD;
     }
 
-    int msub(int a, int b) {
-        return ((0ll + a - b) % MOD + MOD) % MOD;
+    int msub(int64_t a, int64_t b) {
+        return ((a - b) % MOD + MOD) % MOD;
     }
 
-    int mmul(int a, int b) {
-        return 1ll * a * b % MOD;
+    int mmul(int64_t a, int64_t b) {
+        return (a % MOD) * (b % MOD) % MOD;
     }
 
     int mpow(int base, int exp) {
@@ -39,13 +39,11 @@ namespace modular {
         fact.resize(n + 1);
         inv_fact.resize(n + 1);
         fact[0] = fact[1] = inv_fact[0] = inv_fact[1] = 1;
-        
-        for (int i = 2; i <= n; i++) {
+        rng(i, 2, n + 1) {
             fact[i] = mmul(fact[i - 1], i);
         }
-
         inv_fact[n] = minv(fact[n]);
-        for (int i = n - 1; i >= 2; i--) {
+        gnr(i, 2, n) {
             inv_fact[i] = mmul(inv_fact[i + 1], i + 1);
         }
     }
