6ef1d3287b37c894b706b6d8654ddda7cd702075
Mark Zhou <xinyunzhouu@gmail.com>
2024-02-03 10:22:08 -0500

    Update segtree.h

--- data-structures/segtree.h
+++ data-structures/segtree.h
@@ -1,37 +1,36 @@
-template<typename T>
-struct ST {
+template <typename T>
+struct SegmentTree {
     int n;
-    vector<T> tree;
-
     const T id = 0;
+    vector<T> st;
 
     T merge(T a, T b) {
         return a + b;
     }
 
-    ST(int _n) : n(_n), tree(2 * n, id) {}
+    SegmentTree(int _n) : n(_n), st(2 * n, id) {}
 
-    ST(vector<T> &a) : n(a.size()), tree(2 * n) {
+    SegmentTree(vector<T> &a) : n(a.size()), st(2 * n) {
         for (int i = 0; i < n; i++) {
-            tree[i + n] = a[i];
+            st[n + i] = a[i];
         }
         for (int i = n - 1; i > 0; i--) {
-            tree[i] = merge(tree[i << 1], tree[i << 1 | 1]);
+            st[i] = merge(st[i << 1], st[i << 1 | 1]);
         }
     }
- 
+
     T query(int l, int r) {
         T ls = id, rs = id;
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) ls = merge(ls, tree[l++]);
-            if (r & 1) rs = merge(tree[--r], rs);
+            if (l & 1) ls = merge(ls, st[l++]);
+            if (r & 1) rs = merge(st[--r], rs);
         }
         return merge(ls, rs);
     }
- 
+
     void update(int p, T v) {
-        for (tree[p += n] = v, p >>= 1; p > 0; p >>= 1) {
-            tree[p] = merge(tree[p << 1], tree[p << 1 | 1]);
+        for (st[p += n] += v, p >>= 1; p > 0; p >>= 1) {
+            st[p] = merge(st[p << 1], st[p << 1 | 1]);
         }
     }
 };
