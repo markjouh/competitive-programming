3e6282cc7894dc3d8f4db355f5c821a24f93c831
Mark Zhou <xinyunzhouu@gmail.com>
2024-04-09 08:52:36 -0400

    Create chain.h

---
+++ misc/chain.h
@@ -0,0 +1,50 @@
+struct chain {
+    int n;
+    vector<array<int, 2>> ch;
+ 
+    chain(int _n) : n(_n) {
+        ch.resize(n, {-1, -1});
+    }
+ 
+    void insert_l(int x, int y) {
+        assert(x != y);
+        if (ch[y][0] == -1) {
+            ch[y][0] = x;
+            ch[x][1] = y;
+            return;
+        }
+        ch[ch[y][0]][1] = x;
+        ch[x][0] = ch[y][0];
+        ch[x][1] = y;
+        ch[y][0] = x;
+    }
+ 
+    void insert_r(int x, int y) {
+        assert(x != y);
+        if (ch[y][1] == -1) {
+            ch[y][1] = x;
+            ch[x][0] = y;
+            return;
+        }
+        ch[ch[y][1]][0] = x;
+        ch[x][1] = ch[y][1];
+        ch[x][0] = y;
+        ch[y][1] = x;
+    }
+ 
+    vector<int> recover() {
+        vector<bool> vis(n);
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i] && ch[i][0] == -1) {
+                int x = i;
+                while (x != -1) {
+                    res.push_back(x);
+                    vis[x] = true;
+                    x = ch[x][1];
+                }
+            }
+        }
+        return res;
+    }
+};
