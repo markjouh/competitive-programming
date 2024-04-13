bd2e8243cff5f4beb0ae764c93ecedd7c0600b73
Mark Zhou <xinyunzhouu@gmail.com>
2024-04-13 14:02:14 -0400

    Update template.cpp

--- library/language/template.cpp
+++ library/language/template.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#ifdef LOCAL
+#ifdef MARK
 #include "debug.h"
 #else
 #define debug(...)
 #endif
-
-typedef long long ll;
-
-#define all(x) begin(x), end(x)
-#define sz(x) static_cast<int>(size(x))
-
-template<typename T> bool ckmin(T &a, T b) { return b < a ? a = b, 1 : 0; }
-template<typename T> bool ckmax(T &a, T b) { return b > a ? a = b, 1 : 0; }
 
 int main() {
     ios::sync_with_stdio(false);
