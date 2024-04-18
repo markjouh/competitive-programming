5308121483b4ef4c51adc647e9e5d0f14c9ea5d8
Mark Zhou <xinyunzhouu@gmail.com>
2024-04-18 17:56:35 -0400

    Update and rename pbds.h to hash_table.h

--- library/language/hash_table.h
+++ library/language/hash_table.h
@@ -1,6 +1,4 @@
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
@@ -10,14 +8,11 @@ struct custom_hash {
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
- 
+
     size_t operator()(uint64_t x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
     }
 };
 
-// find_by_order(k) - The item at index k
-// order_of_key(k) - Count items < k
-template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<typename K, typename V> using hash_table = gp_hash_table<K, V, custom_hash>;
+template<class K, class V> using hash_table = __gnu_pbds::gp_hash_table<K, V, custom_hash>;
