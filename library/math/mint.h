e6caad287a6cc06b347663bdbc802b992a12c943
Mark Zhou <xinyunzhouu@gmail.com>
2024-04-18 17:48:06 -0400

    Update mint.h
    Change formatting

--- library/math/mint.h
+++ library/math/mint.h
@@ -1,6 +1,6 @@
 struct mint {
     int v;
-    static const int MOD = 1e9 + 7;
+    static const int MOD = 1e9 + 7; // 998244353
 
     mint(long long _v = 0) {
         v = (-MOD < _v && _v < MOD ? _v : _v % MOD);
@@ -21,16 +21,54 @@ struct mint {
         return res;
     }
 
-    mint &operator+=(mint b) { if ((v += b.v) >= MOD) v -= MOD; return *this; }
-    mint &operator-=(mint b) { if ((v -= b.v) < 0) v += MOD; return *this; }
-    mint &operator*=(mint b) { v = 1ll * v * b.v % MOD; return *this; }
-    mint &operator/=(mint b) { v = 1ll * v * pow(b, MOD - 2).v % MOD; return *this; }
+    mint &operator+=(mint b) {
+        if ((v += b.v) >= MOD) {
+            v -= MOD;
+        }
+        return *this;
+    }
 
-    friend mint operator+(mint a, mint b) { return a += b; }
-    friend mint operator-(mint a, mint b) { return a -= b; }
-    friend mint operator*(mint a, mint b) { return a *= b; }
-    friend mint operator/(mint a, mint b) { return a /= b; }
+    mint &operator-=(mint b) {
+        if ((v -= b.v) < 0) {
+            v += MOD;
+        }
+        return *this;
+    }
+
+    mint &operator*=(mint b) {
+        v = 1ll * v * b.v % MOD;
+        return *this;
+    }
+
+    mint &operator/=(mint b) {
+        v = 1ll * v * pow(b, MOD - 2).v % MOD;
+        return *this;
+    }
+
+    friend mint operator+(mint a, mint b) {
+        return a += b;
+    }
+
+    friend mint operator-(mint a, mint b) {
+        return a -= b;
+    }
+
+    friend mint operator*(mint a, mint b) {
+        return a *= b;
+    }
+
+    friend mint operator/(mint a, mint b) {
+        return a /= b;
+    }
     
-    friend istream &operator>>(istream &is, mint &a) { long long x; is >> x; a = mint(x); return is; }
-    friend ostream &operator<<(ostream &os, mint &a) { return os << a.v; }
+    friend istream &operator>>(istream &is, mint a) {
+        long long x;
+        is >> x;
+        a = mint(x);
+        return is;
+    }
+
+    friend ostream &operator<<(ostream &os, mint a) {
+        return os << a.v;
+    }
 };
