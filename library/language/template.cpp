ff1cd37255a7bd0fde4290030f1bd1b2c38af5f3
Mark Zhou <xinyunzhouu@gmail.com>
2024-04-18 17:51:38 -0400

    Update template.cpp

--- library/language/template.cpp
+++ library/language/template.cpp
@@ -2,27 +2,7 @@
 
 using namespace std;
 
-#ifdef LOCAL
-#include "debug.h"
-#else
-#define debug(...)
-#endif
-
-using ll = long long;
-using ld = long double;
-
-#define all(x) begin(x), end(x)
-#define sz(x) static_cast<int>(size(x))
-
-#define pb push_back
-#define eb emplace_back
-#define f first
-#define s second
-
-template<typename T> bool ckmin(T &a, T b) { return b < a ? a = b, 1 : 0; }
-template<typename T> bool ckmax(T &a, T b) { return b > a ? a = b, 1 : 0; }
-
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr);$0
 }
