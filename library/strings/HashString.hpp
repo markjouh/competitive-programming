1a3cddb47462e2388ee31a5c4d2f694b07fc3a12
Mark Zhou <zhoux25@georgeschool.org>
2024-06-06 14:19:30 -0400

    Update and rename hashString.hpp to HashString.hpp

--- library/strings/HashString.hpp
+++ library/strings/HashString.hpp
@@ -6,26 +6,26 @@ const int64_t B = uniform_int_distribution<int64_t>(1E3, MOD - 1E3)(rng);
 
 vector<int64_t> powB = {1};
 
-struct hashPart {
+struct HashPart {
     int64_t value;
     int len;
 
-    hashPart(int64_t x, int y) : value(x), len(y) {}
+    HashPart(int64_t x, int y) : value(x), len(y) {}
 
-    hashPart operator+(hashPart b) {
-        return hashPart((__int128_t(value) * powB[b.len] + b.value) % MOD, len + b.len);
+    HashPart operator+(HashPart b) {
+        return HashPart((__int128_t(value) * powB[b.len] + b.value) % MOD, len + b.len);
     }
 
-    bool operator==(hashPart b) {
+    bool operator==(HashPart b) {
         return value == b.value && len == b.len;
     }
 };
 
 template<class T>
-struct hashString {
+struct HashString {
     vector<int64_t> hash;
 
-    hashString(T s) {
+    HashString(T s) {
         hash.resize(int(s.size()) + 1);
         for (int i = 0; i < int(s.size()); i++) {
             hash[i + 1] = (__int128_t(hash[i]) * B + s[i]) % MOD;
@@ -35,11 +35,11 @@ struct hashString {
         }
     }
 
-    hashPart getHash(int l, int r) {
-        return hashPart(((hash[r] - __int128_t(hash[l]) * powB[r - l]) % MOD + MOD) % MOD, r - l);
+    HashPart getHash(int l, int r) {
+        return HashPart(((hash[r] - __int128_t(hash[l]) * powB[r - l]) % MOD + MOD) % MOD, r - l);
     }
 
-    hashPart getHash() {
+    HashPart getHash() {
         return getHash(0, hash.size() - 1);
     }
 };
