df0765f1e52d3cf33930d0f76b9ee07bf4aba0a3
Mark Zhou <zhoux25@georgeschool.org>
2024-06-08 14:39:38 -0400

    Update ModInt.hpp

--- library/math/ModInt.hpp
+++ library/math/ModInt.hpp
@@ -1,16 +1,16 @@
 template<int MOD>
-struct ModInt {
+struct mod_int {
     int v;
  
-    ModInt(long long _v = 0) {
+    mod_int(long long _v = 0) {
         v = (-MOD < _v && _v < MOD ? _v : _v % MOD);
         if (v < 0) {
             v += MOD;
         }
     }
  
-    friend ModInt pow(ModInt base, int exp) {
-        ModInt res = 1;
+    friend mod_int pow(mod_int base, int exp) {
+        mod_int res = 1;
         while (exp) {
             if (exp & 1) {
                 res *= base;
@@ -21,54 +21,57 @@ struct ModInt {
         return res;
     }
  
-    ModInt &operator+=(ModInt b) {
+    mod_int &operator+=(mod_int b) {
         if ((v += b.v) >= MOD) {
             v -= MOD;
         }
         return *this;
     }
  
-    ModInt &operator-=(ModInt b) {
+    mod_int &operator-=(mod_int b) {
         if ((v -= b.v) < 0) {
             v += MOD;
         }
         return *this;
     }
  
-    ModInt &operator*=(ModInt b) {
+    mod_int &operator*=(mod_int b) {
         v = 1ll * v * b.v % MOD;
         return *this;
     }
  
-    ModInt &operator/=(ModInt b) {
+    mod_int &operator/=(mod_int b) {
         v = 1ll * v * pow(b, MOD - 2).v % MOD;
         return *this;
     }
  
-    friend ModInt operator+(ModInt a, ModInt b) {
+    friend mod_int operator+(mod_int a, mod_int b) {
         return a += b;
     }
  
-    friend ModInt operator-(ModInt a, ModInt b) {
+    friend mod_int operator-(mod_int a, mod_int b) {
         return a -= b;
     }
  
-    friend ModInt operator*(ModInt a, ModInt b) {
+    friend mod_int operator*(mod_int a, mod_int b) {
         return a *= b;
     }
  
-    friend ModInt operator/(ModInt a, ModInt b) {
+    friend mod_int operator/(mod_int a, mod_int b) {
         return a /= b;
     }
     
-    friend istream &operator>>(istream &is, ModInt a) {
+    friend istream &operator>>(istream &is, mod_int a) {
         long long x;
         is >> x;
-        a = ModInt(x);
+        a = mod_int(x);
         return is;
     }
  
-    friend ostream &operator<<(ostream &os, ModInt a) {
+    friend ostream &operator<<(ostream &os, mod_int a) {
         return os << a.v;
     }
 };
+
+// using Z = mod_int<int(1E9 + 7)>;
+// using Z = mod_int<998244353>;
