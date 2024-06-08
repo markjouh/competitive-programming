78b69377da47334d41041ffa90f0d6c4cef94ba9
Mark Zhou <zhoux25@georgeschool.org>
2024-06-08 14:34:57 -0400

    Update and rename mint.h to ModInt.hpp

--- library/math/ModInt.hpp
+++ library/math/ModInt.hpp
@@ -1,16 +1,16 @@
-struct mint {
+template<int MOD>
+struct ModInt {
     int v;
-    static const int MOD = 1e9 + 7; // 998244353
-
-    mint(long long _v = 0) {
+ 
+    ModInt(long long _v = 0) {
         v = (-MOD < _v && _v < MOD ? _v : _v % MOD);
         if (v < 0) {
             v += MOD;
         }
     }
-
-    friend mint pow(mint base, int exp) {
-        mint res = 1;
+ 
+    friend ModInt pow(ModInt base, int exp) {
+        ModInt res = 1;
         while (exp) {
             if (exp & 1) {
                 res *= base;
@@ -20,55 +20,55 @@ struct mint {
         }
         return res;
     }
-
-    mint &operator+=(mint b) {
+ 
+    ModInt &operator+=(ModInt b) {
         if ((v += b.v) >= MOD) {
             v -= MOD;
         }
         return *this;
     }
-
-    mint &operator-=(mint b) {
+ 
+    ModInt &operator-=(ModInt b) {
         if ((v -= b.v) < 0) {
             v += MOD;
         }
         return *this;
     }
-
-    mint &operator*=(mint b) {
+ 
+    ModInt &operator*=(ModInt b) {
         v = 1ll * v * b.v % MOD;
         return *this;
     }
-
-    mint &operator/=(mint b) {
+ 
+    ModInt &operator/=(ModInt b) {
         v = 1ll * v * pow(b, MOD - 2).v % MOD;
         return *this;
     }
-
-    friend mint operator+(mint a, mint b) {
+ 
+    friend ModInt operator+(ModInt a, ModInt b) {
         return a += b;
     }
-
-    friend mint operator-(mint a, mint b) {
+ 
+    friend ModInt operator-(ModInt a, ModInt b) {
         return a -= b;
     }
-
-    friend mint operator*(mint a, mint b) {
+ 
+    friend ModInt operator*(ModInt a, ModInt b) {
         return a *= b;
     }
-
-    friend mint operator/(mint a, mint b) {
+ 
+    friend ModInt operator/(ModInt a, ModInt b) {
         return a /= b;
     }
     
-    friend istream &operator>>(istream &is, mint a) {
+    friend istream &operator>>(istream &is, ModInt a) {
         long long x;
         is >> x;
-        a = mint(x);
+        a = ModInt(x);
         return is;
     }
-
-    friend ostream &operator<<(ostream &os, mint a) {
+ 
+    friend ostream &operator<<(ostream &os, ModInt a) {
         return os << a.v;
     }
 };
