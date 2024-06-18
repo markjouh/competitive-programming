4526b05df0439bf9c444b0bf4b632ec530e8a4ff
Mark Zhou <github.chaps279@passmail.net>
2024-06-18 15:19:14 -0400

    Update sparse_table.h

--- datastructure/sparse_table.h
+++ datastructure/sparse_table.h
@@ -1,18 +1,19 @@
-template<typename T>
+template<class T, T(*op)(T, T)>
 struct sparse_table {
+    int n;
     vector<vector<T>> table;
 
-    sparse_table(vector<T> &a) : table(__lg(sz(a)) + 1, vector<T>(sz(a))) {
+    sparse_table(vector<T> &a) : n(a.size()), table(__lg(n) + 1, vector<T>(n)) {
         table[0] = a;
-        for (int i = 1; i <= __lg(sz(a)); i++) {
-            for (int j = 0; j + (1 << i) <= sz(a); j++) {
-                table[i][j] = min(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
+        for (int i = 1; i <= __lg(n); i++) {
+            for (int j = 0; j + (1 << i) <= n; j++) {
+                table[i][j] = op(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
             }
         }
     }
 
     T query(int l, int r) {
         int i = __lg(r - l);
-        return min(table[i][l], table[i][r - (1 << i)]);
+        return op(table[i][l], table[i][r - (1 << i)]);
     }
 };
