613d01106750e14885965bc8f06aa4802d84b9f4
Mark Zhou <github.chaps279@passmail.net>
2024-06-18 15:24:26 -0400

    Update sparse_table.h
    Waste less memory

--- datastructure/sparse_table.h
+++ datastructure/sparse_table.h
@@ -3,9 +3,10 @@ struct sparse_table {
     int n;
     vector<vector<T>> table;
 
-    sparse_table(vector<T> &a) : n(a.size()), table(__lg(n) + 1, vector<T>(n)) {
+    sparse_table(vector<T> &a) : n(a.size()), table(__lg(n) + 1) {
         table[0] = a;
         for (int i = 1; i <= __lg(n); i++) {
+            table[i].resize(n - (1 << i) + 1);
             for (int j = 0; j + (1 << i) <= n; j++) {
                 table[i][j] = op(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
             }
